82c417a81ac578a61eed1fd14d41526343769911
user <user@LAX>
2021-11-26 22:33:35 +0600

    Add project files.

---
+++ The Last Soldier/Login.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace The_Last_Soldier
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.LeftPanel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.login_bttn_2 = new System.Windows.Forms.Button();
+            this.register_bttn_3 = new System.Windows.Forms.Button();
+            this.login_panel2 = new System.Windows.Forms.Panel();
+            this.register_panel3 = new System.Windows.Forms.Panel();
+            this.LeftPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.register_panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LeftPanel1
+            // 
+            this.LeftPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(106)))), ((int)(((byte)(77)))));
+            this.LeftPanel1.Controls.Add(this.label7);
+            this.LeftPanel1.Controls.Add(this.label6);
+            this.LeftPanel1.Controls.Add(this.label5);
+            this.LeftPanel1.Controls.Add(this.label4);
+            this.LeftPanel1.Controls.Add(this.label3);
+            this.LeftPanel1.Controls.Add(this.pictureBox1);
+            this.LeftPanel1.Controls.Add(this.label2);
+            this.LeftPanel1.Controls.Add(this.label1);
+            this.LeftPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftPanel1.Location = new System.Drawing.Point(0, 0);
+            this.LeftPanel1.Name = "LeftPanel1";
+            this.LeftPanel1.Size = new System.Drawing.Size(405, 667);
+            this.LeftPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(40, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "In War, In Peace We are Everywhere";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(116, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "For Our Country";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(120, 175);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(38, 351);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(326, 35);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Welcome to the Software";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(116, 386);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Feel Free To Work";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(136, 476);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 23);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Develop By";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(107, 499);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(180, 21);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "The Last Soldier Team";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(3, 646);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(219, 21);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Copyright © lastsoldierteam";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(964, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(531, 55);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(376, 23);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Please follow the necessary process to log in.";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(506, 78);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(425, 23);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Only one family member of army man can register.";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // login_bttn_2
+            // 
+            this.login_bttn_2.BackColor = System.Drawing.Color.OliveDrab;
+            this.login_bttn_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login_bttn_2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.login_bttn_2.ForeColor = System.Drawing.Color.White;
+            this.login_bttn_2.Location = new System.Drawing.Point(510, 168);
+            this.login_bttn_2.Name = "login_bttn_2";
+            this.login_bttn_2.Size = new System.Drawing.Size(136, 59);
+            this.login_bttn_2.TabIndex = 7;
+            this.login_bttn_2.Text = "Login";
+            this.login_bttn_2.UseVisualStyleBackColor = false;
+            // 
+            // register_bttn_3
+            // 
+            this.register_bttn_3.BackColor = System.Drawing.Color.OliveDrab;
+            this.register_bttn_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.register_bttn_3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.register_bttn_3.ForeColor = System.Drawing.Color.White;
+            this.register_bttn_3.Location = new System.Drawing.Point(795, 168);
+            this.register_bttn_3.Name = "register_bttn_3";
+            this.register_bttn_3.Size = new System.Drawing.Size(136, 59);
+            this.register_bttn_3.TabIndex = 8;
+            this.register_bttn_3.Text = "Register";
+            this.register_bttn_3.UseVisualStyleBackColor = false;
+            this.register_bttn_3.Click += new System.EventHandler(this.register_bttn_3_Click);
+            // 
+            // login_panel2
+            // 
+            this.login_panel2.BackColor = System.Drawing.Color.White;
+            this.login_panel2.Location = new System.Drawing.Point(0, 0);
+            this.login_panel2.Name = "login_panel2";
+            this.login_panel2.Size = new System.Drawing.Size(421, 410);
+            this.login_panel2.TabIndex = 9;
+            // 
+            // register_panel3
+            // 
+            this.register_panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.register_panel3.Controls.Add(this.login_panel2);
+            this.register_panel3.Location = new System.Drawing.Point(510, 245);
+            this.register_panel3.Name = "register_panel3";
+            this.register_panel3.Size = new System.Drawing.Size(421, 410);
+            this.register_panel3.TabIndex = 0;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1015, 667);
+            this.ControlBox = false;
+            this.Controls.Add(this.register_bttn_3);
+            this.Controls.Add(this.register_panel3);
+            this.Controls.Add(this.login_bttn_2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LeftPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "The Last Soldier";
+            this.LeftPanel1.ResumeLayout(false);
+            this.LeftPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.register_panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel LeftPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button login_bttn_2;
+        private System.Windows.Forms.Button register_bttn_3;
+        private System.Windows.Forms.Panel login_panel2;
+        private System.Windows.Forms.Panel register_panel3;
+    }
+}
+
---
+++ The Last Soldier/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_Last_Soldier
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void register_bttn_3_Click(object sender, EventArgs e)
+        {
+            register_panel3.BringToFront();
+        }
+    }
+}
