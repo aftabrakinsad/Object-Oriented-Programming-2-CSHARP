9f7d7c79d5340ae19ce07e960d2e4275f34cce08
Rakin Saad Aftab <20-41991-1@student.aiub.edu>
2022-05-15 00:34:05 +0600

    Add files via upload

---
+++ Lab Tasks/LabTask1/LabTask1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LabTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implicit
+            int a = 10;
+            char b = 'A';
+
+            //Explicit 
+            double x = 2.22;
+            int y = (int) x; 
+
+            Console.WriteLine("Name: Rakin Sad Aftab");
+            Console.WriteLine("ID: 20-41991-1");
+            Console.WriteLine();
+            Console.Write("Implicit Type Casting: ");
+            Console.WriteLine(a + b);
+            Console.Write("Explicit Type Casting: ");
+            Console.Write(y);
+            Console.ReadKey();
+        }
+    }
+}
