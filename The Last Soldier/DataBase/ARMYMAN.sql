d36b2a1deaddf2f8ee57b91afe91adbd01858528
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2022-07-09 08:17:37 +0600

    Add files via upload

---
+++ The Last Soldier/DataBase/ARMYMAN.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE ARMYMAN (
+Username VARCHAR (50) NOT NULL,
+PASS NVARCHAR (50) NOT NULL,
+Uni_id NVARCHAR (50) NOT NULL,
+Location_A VARCHAR (50) NOT NULL,
+MISSION_NAME VARCHAR (50) NOT NULL,
+Statuss VARCHAR (50) NOT NULL
+);
+
+INSERT INTO ARMYMAN VALUES ('rakinsadaftab', '222222', '20419911', 'Dhaka Cantonment','OML', 'MIA');
+INSERT INTO ARMYMAN VALUES ('saifulislam', '111111', '20425851', 'Chittagong Cantonment','OLMS', 'Active');
+INSERT INTO ARMYMAN VALUES ('nuhanahmed', '333333', '20430681', 'Jessore Cantonment','OTL', 'KIA');
+INSERT INTO ARMYMAN VALUES ('mdamir', '444444', '20000000', 'Postogola Cantonment','OLMS', 'Active');
+
+SELECT * FROM ARMYMAN
